1498406d4a33c00169c89614a4a160059ff7d5a8
AnJ <andrzej.wower@onja.org>
2021-04-15 11:13:24 +0300

    fav2

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { View, StyleSheet, Text, Image } from "react-native"
+import { View, StyleSheet, Text, Image, TouchableOpacity, Platform } from "react-native"
 
 const styles = StyleSheet.create({
     container: {
         padding: 20,
         flexDirection: 'row',
         backgroundColor: '#fff',
-        justifyContent:'space-between',
-        alignItems:'center'
+        justifyContent: 'space-between',
+        alignItems: 'center'
     },
     text: {
         fontSize: 18,
@@ -15,25 +15,48 @@ const styles = StyleSheet.create({
     },
     icon: {
         height: 30,
-        tintColor: '#69696969'
+        tintColor: '#69696969',
+        ...Platform.select({
+            ios: { tintColor: 'blue' },
+            android: { tintColor: 'blue' },
+
+        })
     },
     separator: {
-        flex:1,
-        height:1,
+        flex: 1,
+        height: 1,
         backgroundColor: 'rgba(0,0,0,0.2)'
     }
 })
 
-export const Separator = ()=> <View style={styles.separator}/> 
+export const Separator = () => <View style={styles.separator} />
 
-const ListItem = ({ name }) => {
+const ListItem = ({ name, onFavouritePress, isFavourite }) => {
+
+    let starIcon
+
+    if (isFavourite) {
+        starIcon = Platform.select({
+            ios: require('../assets/icons/ios-star.png'),
+            android: require('../assets/icons/md-star.png'),
+        })
+    } else {
+
+        starIcon = Platform.select({
+            ios: require('../assets/icons/ios-star-outline.png'),
+            android: require('../assets/icons/md-star-outline.png'),
+        })
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.text}>{name}</Text>
-            <Image source={require ('../assets/icons/ios-star-outline.png')}
-        style={styles.icon}
-        resizeMode='contain'
-        />
+            <TouchableOpacity onPress={onFavouritePress}>
+                <Image source={starIcon}
+                    style={styles.icon}
+                    resizeMode='contain'
+                />
+            </TouchableOpacity>
         </View>
     )
 }
