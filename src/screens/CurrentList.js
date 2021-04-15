60c58b6f958154861747926160f60268dd067666
AnJ <andrzej.wower@onja.org>
2021-04-15 06:51:05 +0300

    list

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 
 import nachos from '../data/nachos'
+import ListItem, { Separator } from '../components/ListItem'
+
 export default () => {
     return (
         <SafeAreaView>
-            {nachos.map((item)=>{
-
-                return <Text>{item.name}</Text>
-
-            })}
+            <ScrollView>
+                {nachos.map((item) =>
+                (
+                    <React.Fragment key={item.id}>
+                        <Separator></Separator>
+                        <ListItem  name={item.name} />
+                    </React.Fragment>
+                ))}
+            </ScrollView>
         </SafeAreaView>
 
     )
