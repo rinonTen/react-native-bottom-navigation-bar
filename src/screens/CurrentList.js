fd4dd52e63bc1f0a8ee474b271cc089c05c71ce7
AnJ <andrzej.wower@onja.org>
2021-04-15 14:38:10 +0300

    adding items

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView } from 'react-native'
-
 import nachos from '../data/nachos'
 import ListItem, { Separator } from '../components/ListItem'
 import AddItem from '../components/AddItem'
+import { v4 as uuid } from 'uuid';
 
 export default () => {
+
+    const [list, setList] = useState(nachos)
     return (
-        <SafeAreaView style={{flex: 1 }}>
-            <KeyboardAvoidingView style={{flex: 1 }}>
-                <AddItem />
-                <FlatList data={nachos} renderItem={({ item, index }) => (
+        <SafeAreaView style={{ flex: 1 }}>
+            <KeyboardAvoidingView style={{ flex: 1 }}>
+
+                <FlatList data={list} renderItem={({ item, index }) => (
                     <ListItem name={item.name}
                         onFavouritePress={() => console.log('favvvvv')}
                         isFavourite={index < 2} />
                 )}
                     keyExtractor={(item) => item.id}
                     ItemSeparatorComponent={() => <Separator></Separator>}
+                    ListHeaderComponent={() => <AddItem onSubmitEditing={({ nativeEvent: { text } }) => {
+                        setList([{ id: uuid(), name: text }, ...list])
+                    }} />}
                 />
             </KeyboardAvoidingView>
         </SafeAreaView>)
