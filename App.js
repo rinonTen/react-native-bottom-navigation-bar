40d8b119892e2d2a7eedb966c474b93079f65553
student <rinon.ten@onja.org>
2021-10-20 09:02:19 +0300

    Set up react native

--- App.js
+++ App.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import { Text, View } from 'react-native'
 import Navigation from './src/navigation'
 
-export default () => <Navigation />
+export default function App() {
+ return(
+     <View>
+         <Text>
+             My react Native up
+         </Text>
+     </View>
+ )
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# react-native-bottom-navigation-bar
+Interview assignment
