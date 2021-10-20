2a5d1f60b16d2a9dfdfad3f2a5b2228184abba7d
student <rinon.ten@onja.org>
2021-10-20 11:08:43 +0300

    Remove svgs and replace with pngs

--- App.js
+++ App.js
@@ -1,90 +1,112 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+ 
+import {Image, Text, View} from 'react-native';
 
-import { Text, View } from 'react-native' 
 
-
-function DetailsScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Details Screen</Text>
-        </View>
-    );
+function FavoriteScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text
+        onPress={() => navigation.navigate('Home')}
+        style={{fontSize: 26, fontWeight: 'bold'}}>
+        Favorite Screen
+      </Text>
+    </View>
+  );
 }
 
-function HomeScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the "Home" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
-        </View>
-    );
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text
+        onPress={() => navigation.navigate('Home')}
+        style={{fontSize: 26, fontWeight: 'bold'}}>
+        Details Screen
+      </Text>
+    </View>
+  );
 }
 
-function SettingsScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Settings Screen</Text>
-        </View>
-    );
+function AnotherScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text
+        onPress={() => navigation.navigate('Home')}
+        style={{fontSize: 26, fontWeight: 'bold'}}>
+        Another Screen
+      </Text>
+    </View>
+  );
 }
 
-const homeName = "Home";
-const detailsName = "Details";
-const settingsName = "Settings";
+function ProfileScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text
+        onPress={() => navigation.navigate('Home')}
+        style={{fontSize: 26, fontWeight: 'bold'}}>
+        Profile Screen
+      </Text>
+    </View>
+  );
+}
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text
+        onPress={() => alert('This is the "Home" screen.')}
+        style={{fontSize: 26, fontWeight: 'bold'}}>
+        Home Screen
+      </Text>
+    </View>
+  );
+}
+
+const homeName = 'Home';
+const favoriteName = 'Favorite';
+const detailsName = 'details';
+const settingsName = 'Settings';
+const anotherName = 'Other';
+const profileName = 'Profile';
 
 const Tab = createBottomTabNavigator();
 
+const HomeIcon = () => {
+    const starIcon = Platform.select({
+      ios: require('./src/assets/icons/home.png'),
+      android: require('./src/assets/icons/home.png'),
+    });
+    return <Image source={starIcon} resizeMode="contain" />
+};
+
+
 function MainContainer() {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName={homeName}
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              let rn = route.name;
-  
-              if (rn === homeName) {
-                iconName = focused ? 'home' : 'home-outline';
-  
-              } else if (rn === detailsName) {
-                iconName = focused ? 'list' : 'list-outline';
-  
-              } else if (rn === settingsName) {
-                iconName = focused ? 'settings' : 'settings-outline';
-              }
-  
-              // You can return any component that you like here!
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-          })}
-          tabBarOptions={{
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'grey',
-            labelStyle: { paddingBottom: 10, fontSize: 10 },
-            style: { padding: 10, height: 70}
-          }}>
-  
-          <Tab.Screen name={homeName} component={HomeScreen} />
-          <Tab.Screen name={detailsName} component={DetailsScreen} />
-          <Tab.Screen name={settingsName} component={SettingsScreen} />
-  
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
-   
+
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}>
+        <Tab.Screen
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: () => <HomeIcon />, 
+          }}
+          name={homeName}
+          component={HomeScreen}
+        />
+        <Tab.Screen name={favoriteName} component={FavoriteScreen} />
+        <Tab.Screen name={detailsName} component={DetailsScreen} />
+        <Tab.Screen name={anotherName} component={AnotherScreen} />
+        <Tab.Screen name={profileName} component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default function App() {
- return(
-    <MainContainer/>
- )
+  return <MainContainer />;
 }
