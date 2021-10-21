c161daf1d0ee4bc7e202be0593f43c5b5d1f356c
student <rinon.ten@onja.org>
2021-10-21 15:20:35 +0300

    Remove unused prop in the home screen component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 
-export function HomeScreen({navigation}) {
+export function HomeScreen() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text
