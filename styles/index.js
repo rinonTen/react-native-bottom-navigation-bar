b225e6ed7b6ebf51194efbf2520ae3103f04ee43
student <rinon.ten@onja.org>
2021-10-21 15:42:56 +0300

    Add some paddings around the text

--- styles/index.js
+++ styles/index.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native"
 
 export const screenStyles = StyleSheet.create({
-    container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+    container: {flex: 1, alignItems: 'center', justifyContent: 'center', padding: 32},
     text: {fontSize: 26, fontWeight: 'bold'}
 })
