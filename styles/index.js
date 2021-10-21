740faf2ba7c8909670a48c7cbfa9f9d2789ab6c0
student <rinon.ten@onja.org>
2021-10-21 08:25:13 +0300

    Revert last commits

---
+++ styles/index.js
@@ -0,0 +1,6 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    viewContainer: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+    screenText: {fontSize: 26, fontWeight: 'bold'}
+})
